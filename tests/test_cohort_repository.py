7e79e16011db18eb049f1407e12b321c0d01e41b
Chrise <chall17@hotmail.co.uk>
2023-03-23 11:44:44 +0000

    "changes"

--- tests/test_cohort_repository.py
+++ tests/test_cohort_repository.py
@@ -1,9 +1,11 @@
 from lib.cohort_repository import CohortRepository
 from lib.cohort import Cohort
 from lib.student import Student
+import datetime
 
 """
 When we call CohortRepository#all
+Need to note date entered is datetime.date type
 We get a list of Cohort objects reflecting the seed data.
 """
 def test_get_all_records(db_connection): # See conftest.py to learn what `db_connection` is.
@@ -13,7 +15,7 @@ def test_get_all_records(db_connection): # See conftest.py to learn what `db_con
     cohorts = repository.all() 
 
     assert cohorts == [
-        Cohort(1, "Feb 23", '2023-02-23', [])
+        Cohort(1, "Feb 23", datetime.date(2023, 2, 23), [])
     ]
 
 """
@@ -25,7 +27,7 @@ def test_get_single_record(db_connection):
     repository = CohortRepository(db_connection)
 
     cohort = repository.find(1)
-    assert cohort == Cohort(1, "Feb 23", "2023-02-23")
+    assert cohort == Cohort(1, "Feb 23", datetime.date(2023, 2, 23))
 
 """
 When we call CohortRepository#create
@@ -35,12 +37,12 @@ def test_create_record(db_connection):
     db_connection.seed("seeds/student_directory_2.sql")
     repository = CohortRepository(db_connection)
 
-    repository.create(Cohort(None, "Mar 23", '2023-3-23'))
+    repository.create(Cohort(None, "Mar 23", datetime.date(2023, 2, 23)))
 
     result = repository.all()
     assert result == [
-        Cohort(1, 'Feb 23', '2023-2-23'),
-        Cohort(2, "Mar 23", "2023-03-23")
+        Cohort(1, 'Feb 23', datetime.date(2023, 2, 23)),
+        Cohort(2, "Mar 23", datetime.date(2023, 2, 23))
     ]
 
 """
@@ -61,6 +63,6 @@ def test_find_with_students(db_connection):
     db_connection.seed("seeds/student_directory_2.sql")
     repository = CohortRepository(db_connection)
     cohort = repository.find_with_students(1)
-    assert cohort == Cohort(1, "Feb 23", '2023-02-23', [
+    assert cohort == Cohort(1, "Feb 23", datetime.date(2023, 2, 23), [
         Student(1, "Chris", 1)
     ])
